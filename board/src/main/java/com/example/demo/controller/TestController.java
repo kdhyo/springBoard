0c6905cee4609ada3349ace4bc6f63bb5e066c22
hyo <kdhyo98@gmail.com>
2020-10-19 23:13:33 +0900

    jsp 연동

--- board/src/main/java/com/example/demo/controller/TestController.java
+++ board/src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,35 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
-	@GetMapping(value = "/home")
-	public String home() {
-		return "index.html";
+	@RequestMapping("/main")
+	public String main() throws Exception {
+		return "main";
+	}
+	
+	@RequestMapping("/mav")
+	public ModelAndView mav() throws Exception {
+		ModelAndView mav = new ModelAndView("mavSample");
+		
+		mav.addObject("key", "fruits");
+		
+		List<String> fruitList = new ArrayList<String>();
+		
+		fruitList.add("apple");
+		fruitList.add("orange");
+		fruitList.add("banana");
+		
+		mav.addObject("value", fruitList);
+		
+		return mav;
 	}
 }
