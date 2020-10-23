6cf25d4a848d0922344aea6fc43e3c6acc499aff
kdhyo <kdhyo98@gmail.com>
2020-10-23 14:00:41 +0900

    spring end

--- board/src/main/java/com/example/demo/service/BoardService.java
+++ board/src/main/java/com/example/demo/service/BoardService.java
@@ -8,32 +8,34 @@ import com.example.demo.domain.BoardVO;
 import com.example.demo.domain.PagingVO;
 import com.example.demo.mapper.BoardMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Service("com.example.demo.service.BoardService")
+@RequiredArgsConstructor
 public class BoardService {
 
-	@Resource(name = "com.example.demo.mapper.BoardMapper")
-	BoardMapper mBoardMapper;
+	private final BoardMapper mBoardMapper;
 
 	public PagingVO<BoardVO> boardListService(int nowPage) throws Exception {
-		int total = this.mBoardMapper.boardCount();
+		int total = mBoardMapper.boardCount();
 		PagingVO<BoardVO> page = new PagingVO<BoardVO>(nowPage, total);
 		page.setList(mBoardMapper.boardList(page));
 		return page;
 	}
 
 	public BoardVO boardDetailService(int idx) throws Exception {
-		return this.mBoardMapper.boardDetail(idx);
+		return mBoardMapper.boardDetail(idx);
 	}
 
 	public int boardInsertService(BoardVO board) throws Exception {
-		return this.mBoardMapper.boardInsert(board);
+		return mBoardMapper.boardInsert(board);
 	}
 
 	public int boardUpdateService(BoardVO board) throws Exception {
-		return this.mBoardMapper.boardUpdate(board);
+		return mBoardMapper.boardUpdate(board);
 	}
 
 	public int boardDeleteService(int idx) throws Exception {
-		return this.mBoardMapper.boardDelete(idx);
+		return mBoardMapper.boardDelete(idx);
 	}
 }
